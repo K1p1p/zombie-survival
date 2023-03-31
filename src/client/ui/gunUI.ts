22531242e80b9cfde5163193e4cafa424628e196
Willian Oliveira <willian.silva.oli@gmail.com>
2023-03-31 15:42:52 -0300

    game controls in ui

--- src/client/ui/gunUI.ts
+++ src/client/ui/gunUI.ts
@@ -1,24 +1,34 @@
 import GunModel from "../../model/gun";
 
 export default class GunUI {
-    private getText(gun: GunModel): string {
-        if(gun.isReloading) { return "RELOADING..."; }
+    private getText(gun: GunModel): string[] {
+        if(gun.isReloading) { return [ "RELOADING..." ]; }
 
-        return `${gun.name} - (${gun.fireMode}) - AMMO: ${gun.ammo}/${gun.ammoCapacity}`;
+        return [
+            `${gun.name} (${gun.fireMode})`,
+            `AMMO: ${gun.ammo}/${gun.ammoCapacity}`
+        ]
     }
 
     render(context: CanvasRenderingContext2D, gun: GunModel): void {
         context.resetTransform();
 
-        context.font = "bold 40px Arial";
+        context.font = "bold 25px Arial";
         context.textAlign = "left";
         context.textBaseline = "bottom";
+        context.fillStyle = "#ff0000";
+        context.strokeStyle = "#e60202";
 
-        context.fillStyle = "red";
-        context.fillText(this.getText(gun), 0, document.body.clientHeight);
+        const text: string[] = this.getText(gun);
 
-        context.strokeStyle = "black";
-        context.lineWidth = 1;
-        context.strokeText(this.getText(gun), 0, document.body.clientHeight);
+        context.translate(0, -((text.length - 1) * 30));
+   
+        for (let index = 0; index < text.length; index++) {
+            const txt = text[index];
+            const y = (30 * index);
+
+            context.fillText(txt, 0, document.body.clientHeight + y);
+            context.strokeText(txt, 0, document.body.clientHeight + y);
+        }
     }
 }
